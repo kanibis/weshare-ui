16d161eabf98e773ff46cdfe5bf6fe1dd6ec9c31
Kanishk Bisen <bisenkanishk16@gmail.com>
2022-12-14 23:43:09 +0530

    cleaning and organizing  code

--- src/explore/top-destination/TopDestinations.js
+++ src/explore/top-destination/TopDestinations.js
@@ -1,23 +1,10 @@
 import React from 'react'
 import './TopDestination.css'
-// import Slider from 'react-slick'
-// import "slick-carousel/slick/slick.css"; 
-// import "slick-carousel/slick/slick-theme.css";
 import {imageData} from './data.js' 
 
 
 export default function TopDestinations() {
-    // const settings = {
-    //     dots: true,
-    //     infinite: true,
-    //     speed: 500,
-    //     slidesToShow: 4,
-    //     slidesToScroll: 1,
-        
-    //     nextArrow: "<button className='slick-next slick-arrow'> <i className='fa-solid fa-chevron-right'></i> </button>",
-    //     prevArrow: "<button className='slick-prev slick-arrow'> <i className='fa-solid fa-chevron-left'></i> </button>"
-    // }
-    // console.log(imageData[0].imgLink)
+    
   return (
     <div className='top-destination-container'>
         <h2>Top Destinations</h2>
