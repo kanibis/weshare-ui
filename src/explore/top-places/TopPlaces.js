b4104dd5fc3d43a8ef439738e9e51d2f6b862cab
Kanishk Bisen <bisenkanishk16@gmail.com>
2022-12-15 00:26:25 +0530

    added top place card and carousel

---
+++ src/explore/top-places/TopPlaces.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './TopPlaces.css'
+import {imageData} from './data.js' 
+
+
+export default function TopPlaces() {
+    
+  return (
+    <div className='top-place-container'>
+        <h2>Top Places To Visit</h2>
+        <div className='top-place-image-carousel'>
+            {imageData.map((item) => (
+                    <div key={item.id} className='image-carousel-item'>
+                        <div className="carousel-item-content">
+                            <div className='top-place-image'>
+                                <img src={item.imgLink} alt={item.alt} />
+                            </div>
+                            <h3>{item.city}</h3>
+                            <p>{item.city} is a wonderful destination.</p>
+                            <p style={{color: 'orange'}}>Price: $10,000</p>
+                        </div>       
+                    </div>              
+            ))}
+        </div>
+    </div>
+  )
+}
